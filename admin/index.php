c347aac25f871f8fafcaca54ad6e347fe575dcda
ThaiDuongZ <Thaiduong10a5@gmail.com>
2023-07-21 16:35:06 +0700

    first commit

---
+++ admin/index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mỹ Phẩm M.O.I</title>
+    <link rel="stylesheet" href="./font/fontawesome-free-6.3.0-web/css/all.css">
+    <link rel="stylesheet" href="./font/themify-icons/themify-icons.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <link rel="stylesheet" href="./Css/base.css">
+    <link rel="stylesheet" href="./Css/style.css">
+    <link rel="stylesheet" href="./modules/Qldm/qldm.css">
+    <link rel="stylesheet" href="./modules/product/product.css">
+    <link rel="stylesheet" href="./modules/home/home.css">
+    <link rel="stylesheet" href="./js/style.js">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;700;800;900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="gird">
+        <div class="nav">
+            <div class="nav__header">
+                <img src="./img/ADmin.png" alt="" class="nav__header--img">
+                <img src="./img/Nav_logo.png" alt="" class="nav__header--img0">
+            </div>
+            <div class="nav__body">
+                <div class="nav__body--link">
+                    <a href="" class="nav__body--link_menu">Quản lý danh mục</a>
+                </div>
+                <div class="nav__body--link">
+                    <a href="" class="nav__body--link_menu">Quản lý sản phẩm</a>
+                </div>
+                <div class="nav__body--link">
+                    <a href="" class="nav__body--link_menu">Quản lý giao diện trang chủ</a>
+                </div>
+                <div class="nav__body--link">
+                    <a href="" class="nav__body--link_menu">Thay đổi background Website</a>
+                </div>
+                <div class="nav__body--link">
+                    <a href="" class="nav__body--link_menu">Nhập xuất tồn sản phẩm</a>
+                </div>
+                <div class="nav__body--link">
+                    <a href="" class="nav__body--link_menu">Quản lý khách hàng</a>
+                </div>
+                <div class="nav__body--link">
+                    <a href="" class="nav__body--link_menu">Quản lý đơn hàng</a>
+                </div>
+                <div class="nav__body--link">
+                    <a href="" class="nav__body--link_menu">In hóa đơn</a>
+                </div>
+                <div class="nav__body--link">
+                    <a href="" class="nav__body--link_menu">Báo cáo theo dõi kinh doanh</a>
+                </div>
+                
+            </div>
+        </div>
+        <div class="main">
+            <div class="header">
+                <img src="./img/header.png" alt="" class="header__img">
+            </div>
+            <div class="content"></div>
+        </div>
+    </div>
+</body>
+</html>
